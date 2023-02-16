a66d982995c29112129c49c74edc2c0e9834d876
Aaron <java.jaydev@gmail.com>
2023-02-16 21:27:27 +0900

    react 오프라인 29강 - 글목록보기

--- book-frontend/src/pages/book/Home.js
+++ book-frontend/src/pages/book/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import BookItem from '../../components/BookItem.js';
 
 const Home = () => {
   return (
     <div>
-      <h1>책 목록 보기</h1>
+      <BookItem />
     </div>
   );
 };
